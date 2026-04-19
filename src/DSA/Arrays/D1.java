3d5adaa5cd7d39ce292904e92ab4acb0d903357a
Nikhil <nikhilmagadum68128@gmail.com>
2026-04-19 21:21:45 +0530

    DSA day one

--- src/DSA/Arrays/D1.java
+++ src/DSA/Arrays/D1.java
@@ -5,7 +5,6 @@ public class D1 {
 
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
         int[] marks = new int[5];
         // Basics of array how to declare,initialize and take user input using  for loop
@@ -21,4 +20,7 @@ public class D1 {
 }
 
 // Basic array how to take  input and output
+// Day one of DSA
+// Follwoing Kunal kushwaha
+
 
