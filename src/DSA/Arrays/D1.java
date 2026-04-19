1a4f4e11129b9f7a95a07ab5836112a7407559ce
Nikhil <nikhilmagadum68128@gmail.com>
2026-04-19 21:20:32 +0530

    check

--- src/DSA/Arrays/D1.java
+++ src/DSA/Arrays/D1.java
@@ -5,6 +5,7 @@ public class D1 {
 
     public static void main(String[] args) {
 
+
         Scanner sc = new Scanner(System.in);
         int[] marks = new int[5];
         // Basics of array how to declare,initialize and take user input using  for loop
@@ -20,3 +21,4 @@ public class D1 {
 }
 
 // Basic array how to take  input and output
+
