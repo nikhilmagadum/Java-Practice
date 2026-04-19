a4d67717cc8b0f882428f5cd71274e609934e5b9
Nikhil <nikhilmagadum68128@gmail.com>
2026-04-20 01:51:36 +0530

    understanding the length in array.

---
+++ src/DSA/KK/Arrays/LengthinArray.java
@@ -0,0 +1,14 @@
+package DSA.KK.Arrays;
+
+public class LengthinArray {
+
+    static void main(String[] args) {
+
+//       int [] a  = new int [10];
+//       System.out.println("The length of the array is : " + a.length);
+
+//        int [][] a = new int[2][10];
+//        System.out.println(a[0].length);
+//        System.out.println(a[1].length);
+    }
+}
