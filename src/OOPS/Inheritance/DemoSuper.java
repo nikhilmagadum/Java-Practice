f1ce5c53290ba5fae4d8582203d9115e3aa7a311
Nikhil <nikhilmagadum68128@gmail.com>
2026-04-24 18:13:52 +0530

    learnt how super(); works in java

---
+++ src/OOPS/Inheritance/DemoSuper.java
@@ -0,0 +1,44 @@
+package OOPS.Inheritance;
+
+class Test1 {
+    int x, y;
+
+    Test1() {
+        x = 100;
+        y = 200;
+    }
+
+    Test1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Test2 extends Test1 {
+    int a, b;
+
+    Test2() {
+        a = 300;
+        b = 400;
+    }
+
+    Test2(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    void disp() {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
+public class DemoSuper {
+    public static void main(String[] args) {
+        Test2 t2 = new Test2();
+        t2.disp();
+    }
+
+}
