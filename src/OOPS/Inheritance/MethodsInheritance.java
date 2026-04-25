7aeab6ce943766aab2ed10e6c741241ddb3a9dee
Nikhil <nikhilmagadum68128@gmail.com>
2026-04-25 21:23:15 +0530

    Added few Notes About inherited methods

--- src/OOPS/Inheritance/MethodsInheritance.java
+++ src/OOPS/Inheritance/MethodsInheritance.java
@@ -17,6 +17,7 @@ class Plane {
 
 class CargoPlane extends Plane {
 
+    @Override
     void fly() {
         System.out.println("CargoPlane is flying at Lower Height...");
     }
@@ -27,18 +28,18 @@ class CargoPlane extends Plane {
 }
 
 class PassengerPlane extends Plane {
-
+    @Override
     void fly() {
         System.out.println("Passenger is flying at Medium Height...");
     }
 
-    void carryPassenger(){
+    void carryPassenger() {
         System.out.println("Passenger Plane is carrying passengers....");
     }
 }
 
 class FighterPlane extends Plane {
-
+    @Override
     void fly() {
         System.out.println("Fighter Plane  is flying at Greater  Height...");
     }
@@ -71,3 +72,17 @@ public class MethodsInheritance {
 //        fp.carryWeapons();
     }
 }
+
+
+/*
+what Learnt today is
+types  of methods in inheritance :
+They are Inherited methods--These are the one which will be inherited from other class
+,Overridden methods -- these are the one which same method name as inherited method but used with @Override,
+specilizend methods-- are basically new methods which you create in a class
+
+Annotations : Gives you some Data about methods.
+
+Defination : Annotations in Java are a form of metadata that provide additional information about the program.
+ They do not change the action of a compiled program but can be used by the compiler or runtime for processing.
+ */
