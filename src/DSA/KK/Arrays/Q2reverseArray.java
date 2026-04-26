a497dcbd011add78a79d3293861baab1acfae0f1
Nikhil <nikhilmagadum68128@gmail.com>
2026-04-26 21:37:31 +0530

    Array problems

--- src/DSA/KK/Arrays/Q2reverseArray.java
+++ src/DSA/KK/Arrays/Q2reverseArray.java
@@ -19,6 +19,8 @@ public class Q2reverseArray {
 
         for(int i = ar.length-1; i >= 0 ; i-- ) {
             System.out.println(ar[i]);
+
+            System.out.println("The array elements  ");
         }
     }
 }
