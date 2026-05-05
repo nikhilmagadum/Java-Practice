d5296c8f1b3abf3f5a703a810232ae1231a1df7a
Nikhil <nikhilmagadum68129@gmail.com>
2026-05-05 23:02:32 +0530

    aggregation and compostion both in one code

--- src/OOPS/AggregationComposotion/Charger.java
+++ src/OOPS/AggregationComposotion/Charger.java
@@ -5,7 +5,7 @@ public class Charger {
     String brand;
     float voltage;
 
-    public Charger(String brand,float voltage) {
+    public Charger(String brand, float voltage) {
         this.brand = brand;
         this.voltage = voltage;
     }
@@ -15,43 +15,40 @@ class OS {
     String name;
     float size;
 
-   public OS(String name,float size) {
-        this.name =  name;
+    public OS(String name, float size) {
+        this.name = name;
         this.size = size;
     }
 }
 
 class Mobile {
-    Charger charger; /// creating an instance variable of class Charger
+    Charger charger;
+    /// creating an instance variable of class Charger
     OS os;           // creating an instance variable of class OS
     // has - A relationship is established just by creating the both the variables of those class types
 
-   public Mobile() {
-        os = new OS("Lollipop" ,423.11f);
+    public Mobile() {
+        os = new OS("Lollipop", 423.11f);
     }
 
     // Aggregation is achieved by setters methods
     public void setCharger(Charger charger) {
-       this.charger = charger;
+        this.charger = charger;
     }
-
-
 }
 
 class Main {
     public static void main(String[] args) {
         Mobile MyMobile = new Mobile();
+        Charger myCharger = new Charger("mi", 40.2f);
+
+        MyMobile.setCharger(myCharger);
 
         System.out.println(MyMobile.os.name);
         System.out.println(MyMobile.os.size);
 
-
-       Charger myCharger  =  new Charger("mi",40.2f);
-
-        System.out.println(myCharger.brand);
-        System.out.println(myCharger.voltage);
-
-        MyMobile.setCharger(myCharger);
+        System.out.println(MyMobile.charger.brand);
+        System.out.println(MyMobile.charger.voltage);
 
     }
 }
