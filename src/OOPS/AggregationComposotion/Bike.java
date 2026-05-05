81d04fc548d02b9986f9687fca775125f41b2a89
Nikhil <nikhilmagadum68129@gmail.com>
2026-05-05 23:33:22 +0530

    assoction onemore example of Student and Bike

--- src/OOPS/AggregationComposotion/Bike.java
+++
@@ -1,9 +0,0 @@
-package OOPS.AggregationComposotion;
-
-public class Bike {
-    private String nerves; // As per the text box in your diagram
-
-    public Bike(String nerves) {
-        this.nerves = nerves;
-    }
-}
